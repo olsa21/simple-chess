1edbe5cb07805980d509ffa00bb8f6f2e1bd3fb7
Oliver Sabelfeld <o.sd@tutanota.de>
2022-07-15 22:01:21 +0200

    Start fenster eingefügt

--- schach.py
+++ schach.py
@@ -3,6 +3,7 @@ import random
 import pygame
 import tkinter as tk
 import numpy as np
+import PySimpleGUI as sg
 from tkinter import messagebox
 
 blocks = 8
@@ -37,7 +38,7 @@ class ChessPiece:
 
     def movePiece(self, board, newPosition):
         #Prüfen ob neues Feld erreichbar ist
-        if newPosition not in self.getPossibleMoves():
+        if newPosition not in self.getPossibleMoves(board):
             return False
 
         #Alte Position freigeben
@@ -69,7 +70,7 @@ class ChessPiece:
             self.isFirstMove = False
         return True
 
-    def getPossibleMoves(self):
+    def getPossibleMoves(self, board):
         possibleFields = list()
 
         #print(np.matrix(board.boardArray))
@@ -301,7 +302,7 @@ class Board:
 
         blockWidth = blockSize
 
-        possibleMoves = piece.getPossibleMoves()
+        possibleMoves = piece.getPossibleMoves( board )
 
         #draw positon of the piece
         print("drawing piece at: ", piece.position)
@@ -363,74 +364,93 @@ PawnWhite8 = Pawn((7,6), "white")
 BlackPieceList = [KingBlack, QueenBlack, RookBlack1, RookBlack2, BishopBlack1, BishopBlack2, KnightBlack1, KnightBlack2, PawnBlack1, PawnBlack2, PawnBlack3, PawnBlack4, PawnBlack5, PawnBlack6, PawnBlack7, PawnBlack8]
 WhitePieceList = [KingWhite, QueenWhite, RookWhite1, RookWhite2, BishopWhite1, BishopWhite2, KnightWhite1, KnightWhite2, PawnWhite1, PawnWhite2, PawnWhite3, PawnWhite4, PawnWhite5, PawnWhite6, PawnWhite7, PawnWhite8]
 
+def gameWindow():
+    window = pygame.display.set_mode((screenWidth, screenWidth))
 
-window = pygame.display.set_mode((screenWidth, screenWidth))
+    pygame.display.set_caption("Python Chess")
+    board = Board(WhitePieceList, BlackPieceList)
 
-pygame.display.set_caption("Python Chess")
+    board.drawBoard(window, board)
+    pygame.display.update()
 
+    running = True
+    while running:
+        pygame.time.delay(50)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-
-board = Board(WhitePieceList, BlackPieceList)
-
-board.drawBoard(window, board)
-
-pygame.display.update()
-
-running = True
-while running:
-    pygame.time.delay(50)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if board.isBlackTurn:
-                print("Black Turn")
-            else:
-                print("White Turn")
-
-            pos = pygame.mouse.get_pos()
-            #print(pos)
-
-            #Geklickte Position ermitteln
-            x = pos[0] // (screenWidth // blocks)
-            y = pos[1] // (screenWidth // blocks)
-            #print("x: ", x, " y: ", y)
-
-            pieceOrNone = board.getPiece(x, y)
-            #print(pieceOrNone)
-
-            #Wenn keiner den Fokus hat und Wenn auf dem Feld kein Spieler steht
-            if pieceOrNone == None and board.focusedPiece == None:
-                print("no piece focussed")
-                continue
-
-            #Wenn keine Figur den Fokus hat, fokussieren
-            if board.focusedPiece == None and pieceOrNone != None:
-                #Prüfen ob die ausgewählte Farbe am Zug ist
-                #if pieceOrNone.color == board.isBlackTurn ? "black" : "white":
-                if pieceOrNone.color == "black" if board.isBlackTurn else pieceOrNone.color == "white":
-                    board.focusPieceAndColor(window, pieceOrNone, colorPossible, board)
-                continue
-
-            #Wenn auf dem Feld eine Figur steht
-            if pieceOrNone != None:
-                #Wenn dieselbe Figur gewählt wird, der bereits den Fokus hat, dann Fokus entfernen
-                if board.focusedPiece == pieceOrNone:
-                    board.drawBoard(window, board)
-                    board.focusedPiece = None
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if board.isBlackTurn:
+                    print("Black Turn")
                 else:
-                    #Auf dem Feld steht eine Figur derselben Farbe, Fokus wird gewechselt
-                    if board.focusedPiece.color == board.getPiece(x, y).color:
-                        board.focusPieceAndColor(window, pieceOrNone, colorPossible, board)
-                    else:
-                        #bewegen
-                        if board.focusedPiece.movePiece(board, (x,y)):
-                            board.drawBoard(window, board)
-                        pygame.display.update()
-                continue            
-            else:#Wenn auf dem Feld kein Spieler steht, dann wird der bewegt und der Fokus aufgehoben
-                if  board.focusedPiece.movePiece(board, (x, y)):
-                    board.drawBoard(window, board)
-                    pygame.display.update()
+                    print("White Turn")
+
+                pos = pygame.mouse.get_pos()
+                #print(pos)
+
+                #Geklickte Position ermitteln
+                x = pos[0] // (screenWidth // blocks)
+                y = pos[1] // (screenWidth // blocks)
+                #print("x: ", x, " y: ", y)
+
+                pieceOrNone = board.getPiece(x, y)
+                #print(pieceOrNone)
+
+                #Wenn keiner den Fokus hat und Wenn auf dem Feld kein Spieler steht
+                if pieceOrNone == None and board.focusedPiece == None:
+                    print("no piece focussed")
                     continue
+
+                #Wenn keine Figur den Fokus hat, fokussieren
+                if board.focusedPiece == None and pieceOrNone != None:
+                    #Prüfen ob die ausgewählte Farbe am Zug ist
+                    #if pieceOrNone.color == board.isBlackTurn ? "black" : "white":
+                    if pieceOrNone.color == "black" if board.isBlackTurn else pieceOrNone.color == "white":
+                        board.focusPieceAndColor(window, pieceOrNone, colorPossible, board)
+                    continue
+
+                #Wenn auf dem Feld eine Figur steht
+                if pieceOrNone != None:
+                    #Wenn dieselbe Figur gewählt wird, der bereits den Fokus hat, dann Fokus entfernen
+                    if board.focusedPiece == pieceOrNone:
+                        board.drawBoard(window, board)
+                        board.focusedPiece = None
+                    else:
+                        #Auf dem Feld steht eine Figur derselben Farbe, Fokus wird gewechselt
+                        if board.focusedPiece.color == board.getPiece(x, y).color:
+                            board.focusPieceAndColor(window, pieceOrNone, colorPossible, board)
+                        else:
+                            #bewegen
+                            if board.focusedPiece.movePiece(board, (x,y)):
+                                board.drawBoard(window, board)
+                            pygame.display.update()
+                    continue            
+                else:#Wenn auf dem Feld kein Spieler steht, dann wird der bewegt und der Fokus aufgehoben
+                    if  board.focusedPiece.movePiece(board, (x, y)):
+                        board.drawBoard(window, board)
+                        pygame.display.update()
+                        continue
+
+def menu_window():
+    layout = [
+    [sg.Text('Schachspiel', size=(20, 1),
+             justification='center', font=('Helvetica', 20), key='title')],
+    [sg.Button('Spiel starten', button_color=('white', 'black'), key='start', size=(20, 1)),],
+    ]
+
+    window = sg.Window("Button Click", layout, auto_size_buttons=False,
+                   default_button_element_size=(12, 1), use_default_focus=False, finalize=True)
+
+    while True:
+        event, values = window.read(timeout=100)
+        if event == sg.WINDOW_CLOSED:
+            break
+        if event == 'start':
+            window.close()
+            gameWindow()
+            
+    window.close()
+
+#gameWindow()
+menu_window()
